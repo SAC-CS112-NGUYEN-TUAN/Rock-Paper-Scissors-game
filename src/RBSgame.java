15aadf12d29572a47ab35e26d8aff0ba45c16ed2
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-10-01 02:44:07 -0700

    Fixed Bug on draw game . Problems Tried equalsIgnoreCase, but not work Cpu doesnt repick in Draw game.

--- src/RBSgame.java
+++ src/RBSgame.java
@@ -8,7 +8,10 @@ public class RBSgame {
 		Scanner cont = new Scanner(System.in);	
 		// vs CPU
 		String play ="";
+		
+		
 		do {
+		
 		int computerNum;
 		computerNum = 1 + (int) (Math.random()*3);
 		
@@ -32,42 +35,44 @@ public class RBSgame {
 		System.out.println(CPUpick);
 		
 		String choice =
-				JOptionPane.showInputDialog("Let's play Rock Paper Scissors\nPlease type your Pick\nRock Paper Scissors");
+				JOptionPane.showInputDialog("==========Let's play Rock Paper Scissors=========\nPlease type your Pick\nRock Paper Scissors");
+		do {
+		if (CPUpick.equalsIgnoreCase(choice)){
 		
-		//while loop
-		if (CPUpick.equals(choice)){
-		while (CPUpick.contentEquals(choice)){
-				choice = 
-					JOptionPane.showInputDialog("Draw !!! please type your pick again");
+								choice 
+								=JOptionPane.showInputDialog("Draw !!! please type your pick again");
 		}
 		}
-		else  {
-			if (CPUpick.equals("Rock")) if (choice.equals("Paper")){
+		while (CPUpick.equalsIgnoreCase(choice));
+		
+		 if (!CPUpick.equalsIgnoreCase(choice)) {
+			if (CPUpick.equalsIgnoreCase("Rock")) if (choice.equals("Paper")){
 				JOptionPane.showMessageDialog(null,"Player wins !!!\nCPU picked:"+CPUpick);
 			}
-			else if (CPUpick.equals("Rock")) if (choice.equals("Scissors")){
+			if (CPUpick.equalsIgnoreCase("Rock")) if (choice.equals("Scissors")){
 				JOptionPane.showMessageDialog(null,"CPU wins !!!\nCPU picked:"+CPUpick);
 			}
-			if (CPUpick.equals("Paper")) if (choice.equals("Rock")){
-				JOptionPane.showMessageDialog(null,"CPU wins !!!\nCPU picked:"+CPUpick);
-			}	
-			else if (CPUpick.equals("Paper")) if (choice.equals("Scissors")){
-				JOptionPane.showMessageDialog(null,"Player wins !!!\nCPU picked:"+CPUpick);
-			}
-			if (CPUpick.equals("Scissors")) if (choice.equals("Paper")){
+			if (CPUpick.equalsIgnoreCase("Paper")) if (choice.equals("Rock")){
 				JOptionPane.showMessageDialog(null,"CPU wins !!!\nCPU picked:"+CPUpick);
 			}	
-			else if (CPUpick.equals("Scissors")) if (choice.equals("Rock")){
+			if (CPUpick.equalsIgnoreCase("Paper")) if (choice.equals("Scissors")){
+				JOptionPane.showMessageDialog(null,"Player wins !!!\nCPU picked:"+CPUpick);
+			}
+			if (CPUpick.equalsIgnoreCase("Scissors")) if (choice.equals("Paper")){
+				JOptionPane.showMessageDialog(null,"CPU wins !!!\nCPU picked:"+CPUpick);
+			}	
+			if (CPUpick.equalsIgnoreCase("Scissors")) if (choice.equals("Rock")){
 				JOptionPane.showMessageDialog(null,"Player wins !!!\nCPU picked:"+CPUpick);
 			}
 		}
+		
 		
 	play =
 			JOptionPane.showInputDialog(null,"Do you wanna play again\nType ; Y N");
 		
-	
-		
 		}
-		while (play.equals("y"));
-	}
+		while (play.equalsIgnoreCase("y"));
+		
+		}	
+
 }
