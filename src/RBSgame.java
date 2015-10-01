57058eaceec172516c6c6a66afd26088ba5673c7
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-10-01 00:57:28 -0700

    Tried to convert random to string , i dont know if there is better way , incase there are many more variables to convert. Reason for converting is i want to try use string input from dialog boxes in next step.

--- src/RBSgame.java
+++ src/RBSgame.java
@@ -1,3 +1,4 @@
+import javax.swing.JOptionPane;
 import java.util.Scanner;
 public class RBSgame {
 
@@ -10,9 +11,25 @@ public class RBSgame {
 		int computerNum;
 		computerNum = 1 + (int) (Math.random()*3);
 		
+		//experiment convert random int to string"
+		String CPUpick ="";
+		if(computerNum == 1){
+			CPUpick = "Rock";
+		}
+		else if (computerNum == 2){
+			CPUpick = "Paper";
+		}
+		else if (computerNum == 3){
+			CPUpick = "Scissors" ;
+		}
 		char play;
+		
+		//end of converting
+		
+		
 		//debug
 		//System.out.println(computerNum);
+		//System.out.println(CPUpick);
 		do {
 		int choice;
 		System.out.println("====ROCK PAPER SCISSORS GAME====\nRock = 1, Paper = 2, Scissors = 3\nPlease enter your pick:");
@@ -25,22 +42,22 @@ public class RBSgame {
 		}
 		if (computerNum != choice) {
 			if (computerNum ==1) if (choice == 2){
-				System.out.println("Player win !!! CPU picked "+ computerNum);
+				System.out.println("Player win !!! CPU picked "+ CPUpick);
 			}
 			else if (computerNum ==1) if (choice == 3){
-				System.out.println("CPU win !!! CPU picked "+ computerNum);
+				System.out.println("CPU win !!! CPU picked "+ CPUpick);
 			}
 			if (computerNum == 2) if (choice == 1){
-				System.out.println("CPU win !!! CPU picked "+ computerNum);
+				System.out.println("CPU win !!! CPU picked "+ CPUpick);
 			}	
 			else if (computerNum == 2) if (choice == 3){
-				System.out.println("Player win !!!CPU picked "+ computerNum);
+				System.out.println("Player win !!!CPU picked "+ CPUpick);
 			}
 			if (computerNum == 3) if (choice == 1){
-				System.out.println("Player win !!! CPU picked "+ computerNum);
+				System.out.println("Player win !!! CPU picked "+ CPUpick);
 			}
 			else if (computerNum == 3) if (choice == 2){
-				System.out.println("CPU win !!! CPU picked "+ computerNum);
+				System.out.println("CPU win !!! CPU picked "+ CPUpick);
 			}
 		}
 		
