7d1c0ae10cc5f47a19547f99dd375da93885fb86
SAC-CS112-NGUYEN-TUAN <tmacnguyen0525@gmail.com>
2015-10-01 00:09:22 -0700

    Add player choice to keep playing or not

--- src/RBSgame.java
+++ src/RBSgame.java
@@ -2,41 +2,54 @@ import java.util.Scanner;
 public class RBSgame {
 
 	public static void main(String[] args) {
-				
+			
 		Scanner input = new Scanner(System.in);
+		Scanner cont = new Scanner(System.in);	
 		// vs CPU
 		
 		int computerNum;
 		computerNum = 1 + (int) (Math.random()*3);
-		System.out.println(computerNum);
 		
+		char play;
+		//debug
+		//System.out.println(computerNum);
+		do {
 		int choice;
 		System.out.println("====ROCK PAPER SCISSORS GAME====\nRock = 1, Paper = 2, Scissors = 3\nPlease enter your pick:");
 		choice = input.nextInt();
+		
+		//while loop
 		while (computerNum==choice){
 			System.out.println("Draw !, lets play again\nPlease enter your pick:");
 			choice = input.nextInt();
 		}
 		if (computerNum != choice) {
 			if (computerNum ==1) if (choice == 2){
-				System.out.println("Player win !!!");
+				System.out.println("Player win !!! CPU picked "+ computerNum);
 			}
 			else if (computerNum ==1) if (choice == 3){
-				System.out.println("CPU win !!");
+				System.out.println("CPU win !!! CPU picked "+ computerNum);
 			}
 			if (computerNum == 2) if (choice == 1){
-				System.out.println("CPU win !!");
+				System.out.println("CPU win !!! CPU picked "+ computerNum);
 			}	
 			else if (computerNum == 2) if (choice == 3){
-				System.out.println("Player win !!!");
+				System.out.println("Player win !!!CPU picked "+ computerNum);
 			}
 			if (computerNum == 3) if (choice == 1){
-				System.out.println("Player win !!!");
+				System.out.println("Player win !!! CPU picked "+ computerNum);
 			}
 			else if (computerNum == 3) if (choice == 2){
-				System.out.println("CPU win !!!");
+				System.out.println("CPU win !!! CPU picked "+ computerNum);
 			}
 		}
+		
+	System.out.println("Wanna play again ? \nY / N");
+	play = cont.next().charAt(0);
+		}
+	while( play =='y'); 
+		
+	
 	}
 	}
 
